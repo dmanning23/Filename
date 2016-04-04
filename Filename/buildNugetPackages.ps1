6c873539d8af6bf1c8c8a7a2692244efc928118a
Dan Manning <dmanning23@hotmail.com>
2016-04-03 20:48:37 -0500

    ios & desktopgl nuget pacakges

--- Filename/buildNugetPackages.ps1
+++ Filename/buildNugetPackages.ps1
@@ -1,3 +1,5 @@
 nuget pack .\Filename.Android\Filename.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 
-nuget pack .\Filename.WindowsGL\Filename.WindowsGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\Filename.iOS\Filename.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+
+nuget pack .\Filename.DesktopGL\Filename.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
