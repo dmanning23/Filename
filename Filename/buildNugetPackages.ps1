56f71f7912744942e07578b09c8df1d34b4be8f3
Dan Manning <dmanning23@hotmail.com>
2016-04-22 23:05:31 -0500

    added windows universal proj

--- Filename/buildNugetPackages.ps1
+++ Filename/buildNugetPackages.ps1
@@ -1,3 +1,5 @@
 nuget pack .\Filename.Android\FilenameBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack .\Filename.iOS\FilenameBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack .\Filename.DesktopGL\FilenameBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\Filename.WindowsUniversal\FilenameBuddy.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
