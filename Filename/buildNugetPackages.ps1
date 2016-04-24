df31121ffd8b1eebb1d3892a499fbb0d2f6a9297
Dan Manning <dmanning23@hotmail.com>
2016-04-23 19:48:33 -0500

    moved to pcl nuget

--- Filename/buildNugetPackages.ps1
+++ Filename/buildNugetPackages.ps1
@@ -1,5 +1,2 @@
-nuget pack .\Filename.Android\FilenameBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\Filename.iOS\FilenameBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\Filename.DesktopGL\FilenameBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\Filename.WindowsUniversal\FilenameBuddy.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\FilenameBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget push *.nupkg
